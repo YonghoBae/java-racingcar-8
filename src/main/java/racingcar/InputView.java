dad91d3a8301a9d8e34220da94c80a72430f065d
BaeYongho <dyddyd134@chungbuk.ac.kr>
2025-10-24 19:03:55 +0900

    feat(inputview): 입력값 유효성 검사 추가

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -2,27 +2,49 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class InputView {
     private static final Pattern POSITIVE_INTEGER_PATTERN = Pattern.compile("^[1-9]\\d*$");
 
-    public String inputRacingCarName(){
-        return Console.readLine();
+    public String inputRacingCarName() {
+        String racingCarName = Console.readLine();
+
+        if (!validateRacingCarNames(racingCarName)) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+        }
+
+        return racingCarName;
     }
 
-    public int inputRaceNumber(){
+    private boolean validateRacingCarNames(String racingCarNames) {
+        String[] racingCarNameArray = racingCarNames.split(",");
+
+        return Arrays.stream(racingCarNameArray)
+                .map(String::trim)
+                .allMatch(this::isValidName);
+    }
+
+    private boolean isValidName(String racingCarName) {
+        return !racingCarName.isEmpty() && racingCarName.length() <= 5;
+    }
+
+
+    public int inputRaceNumber() {
         String raceNumberStr = Console.readLine();
 
-        if(!validateRaceNumber(raceNumberStr)){
+        if (!validateRaceNumber(raceNumberStr)) {
             throw new IllegalArgumentException("값은 1 이상의 정수여야 합니다.");
         }
 
         return Integer.parseInt(raceNumberStr);
     }
 
-    private boolean validateRaceNumber(String inputRaceNumber){
-        if(inputRaceNumber == null || inputRaceNumber.isEmpty()){
+    private boolean validateRaceNumber(String inputRaceNumber) {
+        if (inputRaceNumber == null || inputRaceNumber.isEmpty()) {
             return false;
         }
 
