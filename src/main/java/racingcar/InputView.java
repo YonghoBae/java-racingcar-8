4228ffcce519fa8dd76ce2605654be31b4d19fd2
BaeYongho <dyddyd134@chungbuk.ac.kr>
2025-10-25 05:13:46 +0900

    refactor(input): InputView에서 검증로직 제거

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -3,30 +3,15 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private Validator validator;
-
-    public InputView(Validator validator) {
-        this.validator = validator;
-    }
 
     public String inputRacingCarName() {
-        String racingCarName = Console.readLine();
-
-        if (!validator.racingCarNames(racingCarName)) {
-            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
-        }
-
-        return racingCarName;
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
     }
 
-    public int inputRaceNumber() {
-        String raceNumberStr = Console.readLine();
-
-        if (!validator.raceNumber(raceNumberStr)) {
-            throw new IllegalArgumentException("값은 1 이상의 정수여야 합니다.");
-        }
-
-        return Integer.parseInt(raceNumberStr);
+    public String inputRaceRound() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Console.readLine();
     }
 
 }
